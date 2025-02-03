4ce6789dda8882686ef4dd9ccaf3c1843eaceeeb
Beto Koll <albertokoll97@gmail.com>
2025-02-03 20:59:11 -0300

    google-auth

--- src/passport/passport.config.js
+++ src/passport/passport.config.js
@@ -1,6 +1,12 @@
 import passport from 'passport';
 import { Strategy as JwtStrategy } from 'passport-jwt';
 import { JWT_SECRET } from '../utils/index.js';
+import { Strategy as GoogleStrategy } from "passport-google-oauth2";
+import usersModel from '../models/users.model.js';
+
+
+const clientGoogle = "892733688998-e8fdaqpoe9655h99l0u7dje83r05l2as.apps.googleusercontent.com";
+const secretGoogle = "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";
 
 passport.use(
     new JwtStrategy(
@@ -22,3 +28,32 @@ passport.use(
     )
 );
 
+passport.use('google', 
+    new GoogleStrategy({
+        clientID: clientGoogle,
+        clientSecret: secretGoogle,
+        callbackURL: 'http://localhost:3000/api/users/auth/google/callback'
+    }, async (request, accessToken, refreshToken, profile, done) => {
+        try {
+            const userExist = await usersModel.findOne({ email: profile.emails[0]?.value }); //Google me devuelve muchos mails o 1 solo, depende la persona. En un array, entonces si o si 0 para que me de el primero
+            if(userExist){
+                return done(null, userExist);
+            }
+            
+            //Si no existe el user, lo crea.
+            const newUser = {
+                first_name: profile.name.givenName || "",
+                last_name: profile.name.familyName || "",
+                email: profile.emails[0]?.value || "",
+
+                password: "", //Se deja vacio porque se autentica con Google
+            };
+
+            const user = await userModel.create(newUser);
+            return done(null, user)
+
+        } catch (error) {
+            return done(error)
+        }
+    })
+)
